2b0c0155a91e606a31dc57a5c0435aeb1ff91a5d
dahomeykid <dahomeykid@gmail.com>
2025-04-10 18:05:23 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Hello from App!</p>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import connectDB from './config/db.js'
+
+const PORT = process.env.PORT || 3000
+
+connectDB();
+
+const app = express();
+dotenv.config()
+app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the poke-battle api")
+} )
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server 🏃 on port ${PORT}`);
+    
+})
