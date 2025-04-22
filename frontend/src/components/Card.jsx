5b56f9530277190667ff9854e95df58cd8aac6c1
dahomeykid <dahomeykid@gmail.com>
2025-04-22 09:05:45 +0200

    added some fixes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -25,8 +25,8 @@ function Card() {
 
  const handleCatch = (pokemon) => {
     // Get the current caughtPokemons from localStorage
-    if(!caughtPokemons || caughtPokemons === "[]"){
-      // If caughtPokemons is null or empty, initialize it as an empty array{
+    if(!caughtPokemons) {
+      // If caughtPokemons is null or empty, initialize it with the current pokemon
       localStorage.setItem("caughtPokemons", JSON.stringify([pokemon]));
       setCaughtPokemons([pokemon]);
       return;
